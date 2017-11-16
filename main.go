75af46df95e07b58746820a8bdd545d7af20ded8
Le Trung Hieu <hieult1409@gmail.com>
2017-11-16 05:02:25 +0200

    test staticfile

--- main.go
+++ main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"log"
 	"os"
 
-	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,16 +10,15 @@ func main() {
 	port := os.Getenv("PORT")
 
 	if port == "" {
-		log.Fatal("$PORT must be set")
+		//log.Fatal("$PORT must be set")
 		port = "8080"
 	}
 
 	// Create the router
 	router := gin.New()
 	router.Use(gin.Logger())
-
+	router.StaticFile("/", "index.html")
 	router.Static("/app", "frontend")
-	router.Use(static.Serve("/", static.LocalFile("/index.html", false)))
 
 	router.GET("/api/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
