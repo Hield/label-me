8de2549bb53319593ce5d6d3c05834cc0432f1a3
Le Trung Hieu <hieult1409@gmail.com>
2017-11-16 03:28:14 +0200

    First commit local

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+		port = "8080"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	router.GET("/api/", func(c *gin.Context) {
+		c.JSON(200, `
+		{
+			"test": "hola"	
+		}`)
+	})
+	router.Run(":" + port)
+}
